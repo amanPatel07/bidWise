e8d5dcfc06f2066798d89b8579433503960992f0
amanPatel07 <aman.patel0745@gmail.com>
2024-07-17 18:46:46 +0530

    feat: registration api

--- src/controllers/registrationController.ts
+++ src/controllers/registrationController.ts
@@ -1,9 +1,18 @@
+import { sign } from "jsonwebtoken";
 import { User } from "../models/user";
 
+const privateKey = process.env.JWT_TOKEN_KEY || '';
+const expiresIn = process.env.JWT_EXPIRE || '';
+
+function jsonWebToken(id: string) {
+  return sign({ id }, privateKey, {
+    expiresIn: expiresIn
+  });
+}
+
 const registrationControl = async (req: any, res: any) => {
-  console.log(req.body)
   const user = await User.create(req.body);
-  res.status(200).json({user})
+  res.status(200).json({ user })
 }
 
 export default registrationControl;
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,14 +1,40 @@
-import mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { Schema, model } from 'mongoose';
 
-const { Schema } = mongoose;
-
-const UserSchema = new Schema({
+const UserSchema: Schema = new Schema({
   username: {
-    type: Schema.Types.String
+    type: Schema.Types.String,
+    required: true,
+    unique: true
   },
   email: {
+    type: Schema.Types.String,
+    required: true,
+    unique: true
+  },
+  dateOfBirth: {
+    type: Schema.Types.Date,
+    required: true,
+    unique: true
+  },
+  gender: {
     type: Schema.Types.String
+  },
+  password: {
+    type: Schema.Types.String,
+    select: false
+  },
+  confirmPassword: {
+    type: Schema.Types.String,
+    select: false
   }
 });
 
-export const User = mongoose.model('User', UserSchema);
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash((this.password as string | Buffer), 12);
+  this.confirmPassword = undefined;
+  next();
+});
+
+export const User = model('User', UserSchema);
